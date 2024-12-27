e0efc340b055197701f18ded239c0293b70bfe9b
Bui Tran Thien An <buitranthienan1111@gmail.com>
2024-12-28 02:15:45 +0700

    set-up: redux toolkit query

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,61 +1,38 @@
 import { Project, SearchResults, Task, Team, User } from "@/lib/types";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth";
 
 export const api = createApi({
+  // required field
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
-    prepareHeaders: async (headers) => {
-      const session = await fetchAuthSession();
-      const { accessToken } = session.tokens ?? {};
-      if (accessToken) {
-        headers.set("Authorization", `Bearer ${accessToken}`);
-      }
-      return headers;
-    },
   }),
+  // Tên định danh reducer API này trong Redux store
   reducerPath: "api",
+  // Danh sách các "tags" dùng để theo dõi dữ liệu và hỗ trợ các tính năng như cache hoặc invalidate (làm mới)
   tagTypes: ["Projects", "Tasks", "Users", "Teams"],
+  // Định nghĩa các endpoint API
   endpoints: (build) => ({
-    getAuthUser: build.query({
-      queryFn: async (_, _queryApi, _extraoptions, fetchWithBQ) => {
-        try {
-          const user = await getCurrentUser();
-          const session = await fetchAuthSession();
-          if (!session) throw new Error("No session found");
-          const { userSub } = session;
-          const { accessToken } = session.tokens ?? {};
-
-          const userDetailsResponse = await fetchWithBQ(`users/${userSub}`);
-          const userDetails = userDetailsResponse.data as User;
-
-          return { data: { user, userSub, userDetails } };
-        } catch (error: any) {
-          return { error: error.message || "Could not fetch user data" };
-        }
-      },
-    }),
     getProjects: build.query<Project[], void>({
-      query: () => "projects",
-      providesTags: ["Projects"],
+      query: () => "projects", // Tương ứng với API: GET /projects
+      providesTags: ["Projects"], // Đánh dấu dữ liệu lấy về liên quan đến "Projects"
     }),
+    // POST, PUT, DELETE (Các method thay đổi dữ liệu trên Server) là mutation
     createProject: build.mutation<Project, Partial<Project>>({
       query: (project) => ({
-        url: "projects",
+        url: "projects", // Tương ứng với API: POST /projects
         method: "POST",
         body: project,
       }),
+      // Xác định tag nào sẽ bị invalidate (làm mới cache) sau khi mutation này được thực hiện
+      // Giúp đồng bộ hóa dữ liệu
       invalidatesTags: ["Projects"],
     }),
-    getTasks: build.query<Task[], { projectId: number }>({
-      query: ({ projectId }) => `tasks?projectId=${projectId}`,
-      providesTags: (result) =>
-        result
-          ? result.map(({ id }) => ({ type: "Tasks" as const, id }))
-          : [{ type: "Tasks" as const }],
+    getTasks: build.query<Task[], void>({
+      query: () => "tasks", // Tương ứng với API: GET /tasks
+      providesTags: ["Tasks"],
     }),
-    getTasksByUser: build.query<Task[], number>({
-      query: (userId) => `tasks/user/${userId}`,
+    getUserTasks: build.query<Task[], number>({
+      query: (userId) => `tasks/user/${userId}`, // Tương ứng với API: GET /tasks/user/${userId}
       providesTags: (result, error, userId) =>
         result
           ? result.map(({ id }) => ({ type: "Tasks", id }))
@@ -79,29 +56,7 @@ export const api = createApi({
         { type: "Tasks", id: taskId },
       ],
     }),
-    getUsers: build.query<User[], void>({
-      query: () => "users",
-      providesTags: ["Users"],
-    }),
-    getTeams: build.query<Team[], void>({
-      query: () => "teams",
-      providesTags: ["Teams"],
-    }),
-    search: build.query<SearchResults, string>({
-      query: (query) => `search?query=${query}`,
-    }),
   }),
 });
 
-export const {
-  useGetProjectsQuery,
-  useCreateProjectMutation,
-  useGetTasksQuery,
-  useCreateTaskMutation,
-  useUpdateTaskStatusMutation,
-  useSearchQuery,
-  useGetUsersQuery,
-  useGetTeamsQuery,
-  useGetTasksByUserQuery,
-  useGetAuthUserQuery,
-} = api;
+export const { useGetProjectsQuery } = api;
